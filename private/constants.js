122961267438c15764aeaa09824ff81a6e480a41
esommer <emily.sommer@gmail.com>
2013-11-19 11:26:17 -0500

    fixed constants

--- private/constants.js
+++ private/constants.js
@@ -2,7 +2,7 @@
 module.exports = {
 	client_id : '9a15cbfd59fe4507bba20dcf44092fa4',
 	client_secret : 'f114ad33a7764d4a9205f8966ab84b0d',
-	address : 'emilys-macbook-pro.local:3000',
+	address : 'emilys-macbook-pro.local:5000',
 	cookieName : 'instaGrabber session saver',
 	cookieSecret : '000verytopsecretsecret000',
 	userFile : './private/users.json'
