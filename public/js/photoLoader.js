4d975e9799a1ca0fd0a83cf8902249c81a2137af
esommer <emily.sommer@gmail.com>
2013-12-02 11:04:31 -0500

    debugging zipping code, logging selected files

--- public/js/photoLoader.js
+++ public/js/photoLoader.js
@@ -50,6 +50,8 @@ window.onload = function () {
 	Messenger.prototype = {
 		send : function (to, dataObj, cbScope, callback) {
 			console.log('req sent to '+ to);
+			console.log('data: ');
+			console.log(dataObj);
 			var url = this.window.location.href.replace(this.window.location.href.replace(/^http:\/\/[\w]*\//, ''), '') + '/' + to;
 			var requestObj = new XMLHttpRequest();
 			var data = {};
@@ -225,6 +227,8 @@ window.onload = function () {
 					files.push(fileObjs[i].value);
 				}
 			}
+			console.log('grabbed selected imgs: ');
+			console.log(files);
 			return files;
 		},
 		// resizeImgs : function () {
@@ -396,24 +400,24 @@ window.onload = function () {
 		getFirstPhotos : function () {
 			var loaded = false;
 			var photos = [];
-			// if (this.localStorage.photos !== undefined) {
-			// 	try {
-			// 		photos = JSON.parse(this.localStorage.photos);
-			// 		loaded = true;
-			// 	}
-			// 	catch (e) {
-			// 		if (e) console.log('localStorage failed to parse');
-			// 	}
-			// }
-			// if (loaded === true) {
-			// 	this.dom.displayPhotos(photos);
-			// 	this.notifier.emit('all photos loaded',{'totalMedia':photos.length});
-			// }
-			// else if (loaded === false) {
+			if (this.localStorage.photos !== undefined) {
+				try {
+					photos = JSON.parse(this.localStorage.photos);
+					loaded = true;
+				}
+				catch (e) {
+					if (e) console.log('localStorage failed to parse');
+				}
+			}
+			if (loaded === true) {
+				this.dom.displayPhotos(photos);
+				this.notifier.emit('all photos loaded',{'totalMedia':photos.length});
+			}
+			else if (loaded === false) {
 				this.msg.send('photos', {'action':'getPhotos','requestNum':0}, this.msg, function (data) {
 					this.photoLoader(data);
 				});
-			//}
+			}
 		},
 		getMorePhotos : function (req) {
 			this.msg.send('photos', req, this.msg, function (data) {
